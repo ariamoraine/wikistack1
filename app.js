f9a421c71c68eb784d4510ce93971302bdc5d8b8
Aria Moraine <ariamoraine@gmail.com>
2017-01-26 22:30:15 -0500

    added in DB

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const model = require('./models');
+const Page = model.Page;
+const User = model.User;
 
 var app = express();
 
@@ -20,6 +23,12 @@ app.get('/', function(req, res, next){
   res.render('index');
 });
 
-app.listen(3000, function(){
-  console.log('server is listeing on port 3000');
-});
+User.sync()
+  .then(function() {
+    return Page.sync();
+  })
+  .then(function() {
+    app.listen(3000, function(){
+      console.log('server is listeing on port 3000');
+    });
+  });
---
+++ models/index.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack1');
+
+var Page = db.define('page', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  urlTitle: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  content: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  status: {
+    type: Sequelize.ENUM('open', 'closed')
+  }
+});
+
+var User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    isEmail: true
+  }
+});
+
+module.exports = {
+  Page: Page,
+  User: User
+};
